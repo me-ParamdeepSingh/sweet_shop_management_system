4cbf6583a5a8698926dd4011091a63c580bec7d6
Paramdeep Singh <=>
2025-12-14 16:23:45 +0530

    feat: prevent creating sweet with negative quantity

--- backend/src/controllers/sweetController.js
+++ backend/src/controllers/sweetController.js
@@ -7,11 +7,19 @@ class sweetController{
 
         // Temporary: To bypass the DB for TDD green case
         if(process.env.NODE_ENV === "test"){   
+            if(quantity < 0){
+                return res.status(400).json({message: "quantity must be valid number"})
+            }
             return res.status(201).json({name,category,price,quantity});
         }
         
+
         // Actual production code
         const newSweet = new sweetModel({name,category,price,quantity});
+        
+        if(quantity < 0){
+            return res.status(400).json({message: "quantity must be valid number"})
+        }
         const savedSweet = await newSweet.save();
         
         return res.status(201).json({sweet: savedSweet});
